b4a1d0861087eab3583866d7e3bf562f0fd3f7fc
paopao1121 <paopao_1121@163.com>
2019-04-25 20:05:56 +0800

    part of login

--- dglogin/settings.py
+++ dglogin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dglogin',
+    #'login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
--- login/views.py
+++ login/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render, redirect
+from . import models
+from login.forms import UserForm
 # Create your views here.
 def index(request):
     pass
     return render(request, 'login/index.html')
 
+
 def login(request):
-    pass
-    return render(request, 'login/login.html')
+    if request.method == "POST":
+        login_form = UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户不存在！"
+        return render(request, 'login/login.html', locals())
+
+    login_form = UserForm()
+    return render(request, 'login/login.html', locals())
 
 def register(request):
     pass
