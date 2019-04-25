5e5b85c2f252dfb9cba2adfc18357081ff12788f
paopao1121 <paopao_1121@163.com>
2019-04-25 15:21:37 +0800

    part of login

--- dglogin/settings.py
+++ dglogin/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',       #数据库名字
         'USER': 'root',         #账号
-        'PASSWORD': '123456',   #密码
+        'PASSWORD': 'root',   #密码
         'HOST': '127.0.0.1',    #IP
         'PORT': '3306',         #端口
     }
@@ -124,3 +124,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
